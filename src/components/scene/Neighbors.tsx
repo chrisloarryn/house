4ec7b44d15ae32e265d71a688566d3f133175f7a
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2026-05-06 19:45:42 -0400

    Add perimeter walls to neighbor houses

--- src/components/scene/Neighbors.tsx
+++ src/components/scene/Neighbors.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ExistingHouse } from './ExistingHouse';
 import { Roof } from './Roof';
+import { Perimeter } from './Perimeter';
 import { L } from '../../config/constants';
 import type { Palette } from '../../config/palettes';
 
-// Reusing ExistingHouse and Roof, just translated
+// Reusing ExistingHouse, Roof, and now Perimeter for neighbor houses
 export function Neighbors({ palette }: { palette: Palette }) {
   const f = L.lote.frente;
 
@@ -13,11 +14,14 @@ export function Neighbors({ palette }: { palette: Palette }) {
       <group position={[-f, 0, 0]}>
         <ExistingHouse palette={palette} isActual={true} />
         <Roof palette={palette} isActual={true} />
+        <Perimeter />
       </group>
       <group position={[f, 0, 0]}>
         <ExistingHouse palette={palette} isActual={true} />
         <Roof palette={palette} isActual={true} />
+        <Perimeter />
       </group>
     </group>
   );
 }
+
